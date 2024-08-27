9649b3b9bbf90bb8cdd67d0a97fb6c4c8b44c062
Tina Schmidtbauer <tina.schmidtbauer@bolasys.de>
2024-08-27 11:14:21 +0200

    React-E-Mail + sweetalert

--- app/kontakt/page.tsx
+++ app/kontakt/page.tsx
@@ -1,16 +1,37 @@
 "use client"
 
 import Header from "@/components/header";
-import { RadioGroup,Radio, Textarea } from "@nextui-org/react";
+import { RadioGroup,Radio, Textarea, Input} from "@nextui-org/react";
 import { Button } from "@nextui-org/react";
-import { useRouter } from "next/navigation";
+import swal from "sweetalert";
+
+import { sendEmail } from "@/actions";
+import { useEffect } from "react";
+import { useFormState } from "react-dom";
+
 
 /* E-Mail senden: https://dev.to/shahmir049/how-to-send-emails-using-nextjs-14-resend-and-react-email-2b7g */
 /* https://dev.to/onlyoneerin/how-to-create-and-send-custom-emails-using-react-email-and-resend-api-4fc0 */
  
+/* Api Key: re_b4SRtHMr_DQsHbNDt7ctJ7KoesXYGe7VG */
 const KontaktPage = () =>{
 
-  const router = useRouter();
+  const [sendEmailState, sendEmailAction] = useFormState(sendEmail, {
+    error:null,
+    success:false
+  })
+
+  useEffect(() => {
+    if (sendEmailState.success)
+    {
+      swal({title:"Erfolg!",text:"E-Mail wurde gesendet!",icon:"success"})
+    }
+    if (sendEmailState.error)
+    {
+      swal({title:"Fehler!",text:"E-Mail konte nicht gesendet werden!",icon:"warning",dangerMode:true})
+    }
+  }, [sendEmailState])
+
   const bereiche = [
     {key: "maschine",label: "Maschine"},
     {key: "software",label: "Software"},
@@ -20,33 +41,56 @@ const KontaktPage = () =>{
     <div style={{marginLeft:"50%"}}>
         <Header titel="Kontakt" opacity="100%"/>
         <div style={{marginLeft:"-100%",width:"400px"}} className="container h-full w-full flex flex-col py-8">
-        <form>
+       
+        
+          <form action={sendEmailAction}>
         <table className="tab">
           <tbody>
+           <tr><td colSpan={2}>
+            <Input 
+            isRequired type="username" 
+            name="username"
+            label="Username" 
+            defaultValue="" 
+            className="max-w-xs" 
+            variant="bordered"/></td><td></td></tr>
+           
+           <tr><td colSpan={2}> 
+            <Input 
+            isRequired type="email" 
+            name="email"
+            label="E-Mail" 
+            defaultValue="" 
+            className="max-w-xs" 
+            variant="bordered"/></td><td></td></tr>
+            
             <tr><td colSpan={2}>
-             <RadioGroup label="Wählen Sie ihren Bereich aus." color="warning" defaultValue="GPE">
-             <Radio value="gpe">GPE</Radio>
-              <Radio value="designer">Designer</Radio>
-              <Radio value="cydesigner">CyDesigner</Radio>
-              <Radio value="nesting">NestingSoftware</Radio>
-              <Radio value="sonstiges">Sonstiges</Radio>
-             </RadioGroup>
+             <RadioGroup label="Wählen Sie ein Thema aus." color="warning" defaultValue="GPE">
+             <Radio value="gpe" name="thema">GPE</Radio>
+              <Radio value="designer" name="thema">Designer</Radio>
+              <Radio value="cydesigner" name="thema">CyDesigner</Radio>
+              <Radio value="nesting" name="thema">NestingSoftware</Radio>
+              <Radio value="sonstiges" name="thema">Sonstiges</Radio>
+             </RadioGroup></td><td></td></tr>
+             
              <tr><td height={30}></td><td></td></tr>
-              </td><td></td></tr>
+            
             <tr><td colSpan={2}><Textarea
-            value=""
-              label=""
+              name="nachricht"
               placeholder="Fragen Sie uns etwas !"
               className="max-w-xs"
-              labelPlacement="outside"
               variant="faded"
               /></td><td></td></tr>
+              
               <tr><td height={30}></td><td></td></tr>
+        
         <tr><td></td><td><Button type="submit" className="bg-[#5ec4d2] text-black mt-2">Absenden</Button></td></tr>
         </tbody>
-        </table>
-          
+        </table>    
         </form>
+        
+          
+       
         </div>
       
         </div>
