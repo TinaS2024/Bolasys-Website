ffb6e6540edd8957fff978919bad859ae0541024
Tina Schmidtbauer <tina.schmidtbauer@bolasys.de>
2024-09-17 12:58:05 +0200

    toReverse wieder rausgenommen, weil Fehlerhaft.

--- app/api/posts/route.ts
+++ app/api/posts/route.ts
@@ -1,42 +1,23 @@
-import { NextRequest } from "next/server";
 import fsPromises from "fs/promises";
 import path from "path";
 
 export async function POST(req : Request)
-//export default async function POST(req:NextRequest) 
 {
     const {artikel} = await req.json()
     console.log("artikel", JSON.stringify(artikel))
-    //if (req.method ==="POST")
-    {
-        const dataFilePath = path.join(process.cwd()+"/app/json/blog.json");
-        console.log("dataFilePath", dataFilePath)
-        //const dataFilePath = "../../json/blog.json";
-        const jsonData = await fsPromises.readFile(dataFilePath);
-        const objectData = JSON.parse(jsonData.toString());
-    
-        //const {titel,subtitel,inhalt,datum} = await req.json();
 
-/*         const newData = {
-            artikellist: [{  
-                        
-            titel:titel,
-            subtitel: subtitel,
-            inhalt: inhalt,
-            datum: datum,
-            }]
-            };
-        objectData.push(newData);
- */       
-        objectData.artikellist.push(artikel);
-        const updatedData = JSON.stringify(objectData, null, 2);
+    const dataFilePath = path.join(process.cwd()+"/app/json/blog.json");
+    console.log("dataFilePath", dataFilePath)
+    const jsonData = await fsPromises.readFile(dataFilePath);
+    const objectData = JSON.parse(jsonData.toString());  
+    objectData.artikellist.push(artikel);
+    const updatedData = JSON.stringify(objectData, null, 2);
 
-        await fsPromises.writeFile(dataFilePath,updatedData);
-
-        //return Response.json({ titel,subtitel,inhalt,datum})   
-        return Response.json(artikel)   
-        }
+    await fsPromises.writeFile(dataFilePath,updatedData);
+ 
+    return Response.json(artikel)   
+}
         
     
-    }  
+    
 
--- app/blogsite/page.tsx
+++ app/blogsite/page.tsx
@@ -20,42 +20,17 @@ export default async function Blogsite({posts}:UserProps) {
   const res = await fsPromises.readFile(process.cwd() + "/app/json/blog.json","utf8")
   posts = JSON.parse(res.toString());
   Response.json(posts),{status:200};
-/*
-const list = [
-    {
-      titel: "Open Ai, ChatGPT",
-      subject: "Künstliche Intelligenz",
-      content:"OpenAI ist ein ursprünglich als Non-Profit-Organisation gegründetes und heute auch kommerziell agierendes Unternehmen.\n Es beschäftigt sich mit der Erforschung und Entwicklung von Künstlicher Intelligenz (KI).\n Bekannt wurde OpenAI durch Projekte wie den KI-Bildgenerator DALL-E oder den Chatbot ChatGPT.\n Anfang 2023 wurde bekannt, dass Microsoft eine mehrjährige, milliardenschwere Investition in OpenAI plant.",
-      bildpfad: image1,
-      date: "01.09.2024"
-    },
-    {
-      titel: "W3.CSS",
-      subject: "Web-Programmierung",
-      content: "W3.CSS ist ein modernes CSS-Framework mit eingebauter Reaktionsfähigkeit:\n \n • Kleiner und schneller als andere CSS-Frameworks.\n • Einfacher zu erlernen und einfacher zu verwenden als andere CSS-Frameworks. \n • Verwendet nur Standard-CSS (keine jQuery- oder JavaScript-Bibliothek).\n • Beschleunigt und vereinfacht die Webentwicklung.\n • Unterstützt standardmäßig modernes responsives Design (mobile first). \n • Sorgt für CSS-Gleichheit in allen Browsern. Chrome, Firefox, IE, Safari, und mehr. \n • Bietet CSS-Gleichheit für alle Geräte. PC, Laptop, Tablet und Handy:Geben Sie auf der linken Seite etwas Markdown ein. Sehen Sie HTML auf der rechten Seite",
-      bildpfad: image2,
-      date: "30.08.2024"
-    },
-  ]
 
-  {list.map((item) => (
-    item.content.split("\n")
-  ))}*/
-  
   return (  
 
 <>
 <div style={{marginLeft:"50%",marginTop:"-150px"}}>   
 <Header opacity="100%" titel="Bolasys Blog"></Header>
 </div>
-{/*
-{list.map((item, index) => (
-  <Blog_Artikel className="line" key={index} titel={item.titel} subject={item.subject} bildpfad={item.bildpfad} content={item.content} date={item.date}></Blog_Artikel>
-))}
-*/}
 
 {
-posts.artikellist.toReversed().map((post:any) =>
+  //toReversed
+posts.artikellist.map((post:any) =>
 (
   <Blog_Artikel key={post.id} className="line" titel={post.titel} subject={post.subtitel} bildpfad={img_platzhalter} content={post.inhalt} date={post.datum}/>
 ))
