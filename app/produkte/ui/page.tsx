f71503e87d29bb07030a674867e9ee1717da17a9
TinaS2024 <Tina.Schmidtbauer@bolasys.de>
2025-09-05 12:18:50 +0200

    Direktimport

--- app/produkte/ui/page.tsx
+++ app/produkte/ui/page.tsx
@@ -13,9 +13,6 @@ import leerbild from "@/public/leerbild.png";
 
 export default function UI() {
 
-  const bild1 = magento_bild;
-  const bild2 = next_bild;
-
   const text0 =["Wir können User-Interfaces mit Java-Script-Frameworks erstellen und Sie in CMS-Systeme einbinden."];
   const text1 = ["Layout-Design mit Page-Builder und Content-Pflege."];
   const text2 = ["Landingpages mit dem neuen React-Java-Script-Framework Next."];
@@ -28,9 +25,9 @@ export default function UI() {
 
           <Titel_Subtitel_nolink titel="UI" subtitel={text0}/>  
   
-          <div id="magento" className="relative flex flex-col items-center mb-8"><DesignerTooltip top="4%" left="4%" titel="Magento" beschreibung={text1} bildpfad={leerbild}/><Image src={bild1.src} alt="magento" width={200} height={200}></Image></div>
+          <div id="magento" className="relative flex flex-col items-center mb-8"><DesignerTooltip top="4%" left="4%" titel="Magento" beschreibung={text1} bildpfad={leerbild}/><Image src={magento_bild} alt="magento" width={200} height={200}></Image></div>
           <p>{text1}</p>
-          <div id="next" className="relative flex flex-col items-center"><DesignerTooltip top="5%" left="-5%" titel="Next JS" beschreibung={text2} bildpfad={leerbild}/><Image src={bild2.src} alt="next-js" width={200} height={200}></Image></div>
+          <div id="next" className="relative flex flex-col items-center"><DesignerTooltip top="5%" left="-5%" titel="Next JS" beschreibung={text2} bildpfad={leerbild}/><Image src={next_bild} alt="next-js" width={200} height={200}></Image></div>
           <p>{text2}</p>
     </>
     );
